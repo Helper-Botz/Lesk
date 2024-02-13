403c70fde95724ac5b187a01e2ae3aeab5980e51
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-13 16:03:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Helper-Botz/MINNALMURALI.git /MINNALMURALI
+  git clone https://github.com/Helper-Botz/LILSA.git /LILSA
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MINNALMURALI
+  git clone $UPSTREAM_REPO /LILSA
 fi
-cd /MINNALMURALI
+cd /LILSA
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 lilsa.py

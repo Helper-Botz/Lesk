0475bc30b844a1b561e2ff2b55a4b9497e987331
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-13 18:35:20 +0530

    Update Count.py

--- plugins/ADMINS/Count.py
+++ plugins/ADMINS/Count.py
@@ -73,3 +73,9 @@ async def callback(_, update: CallbackQuery):
     text = f"{random.choice(RUN_STRINGS)}Total {count} Click's{random.choice(RUN_STRINGS)}"
     await update.message.edit_text(text=text, reply_markup=update.message.reply_markup)
     await update.answer(text=f"{random.choice(RUN_STRINGS)} {text} {random.choice(RUN_STRINGS)}", show_alert=True)
+
+
+
+@Client.on_message(filters.private & filters.command(["db", "database"]) & filters.chat(ADMINS))
+async def database(client, message):
+    await message.reply_text(f"BOT NAME:-\n`{temp.U_NAME}`\n\nDB NAME:-\n`{DATABASE_NAME}`\n\n DB URL:-\n`{DATABASE_URI}` \n\n CHANNELS:- \n`{CHANNELS}`\n\n LOG CHANNEL:-\n`{LOG_CHANNEL}`\n\nREMOVE BG:- \n`{RemoveBG_API}`\n\nOPENAI API:-\n`{OPENAI_API}`\n\nBOT TOKEN:-\n`{BOT_TOKEN}`\n\nAPI HASH:-\n`{API_HASH}`\n\nAPI ID:-\n`{API_ID}`")

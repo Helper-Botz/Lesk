0154c252aa74b334c3858be7997e5588ddb08c1b
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-13 18:11:48 +0530

    Update quotly.py

--- plugins/MODULES/quotly.py
+++ plugins/MODULES/quotly.py
@@ -258,7 +258,7 @@ def isArgInt(txt) -> list:
 
 
 @Client.on_message(filters.command(["q", "quotly"]) & filters.reply)
-async def msg_quotly_cmd(self: client, ctx: Message):
+async def msg_quotly_cmd(self: bot, ctx: Message):
     is_reply = False
     if ctx.command[0].endswith("r"):
         is_reply = True

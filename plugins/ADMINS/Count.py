a97ee400cdc38a882a2d7e87d914098b5a7e85af
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-24 15:11:16 +0530

    Add files via upload

--- plugins/ADMINS/Count.py
+++ plugins/ADMINS/Count.py
@@ -11,9 +11,13 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import MessageNotModified
 from dotenv import load_dotenv
 import random
-from info import ADMINS, SUPPORT_CHAT_ID, SUPPORT_CHAT_RULES, MOVIE_RULES, UPDATE_CHANNEL_ID, PRINT_ID, GRP_LNK, LOW_SIZE, SUPPORT_CHAT
+from plugins.helpers.config import *
+from info import *
+from database.utils import temp
 
 
+# OKDAA = OPENAI_API RemoveBG_API BOT_NAME ADMINS SUPPORT_CHAT_ID SUPPORT_CHAT_RULES MOVIE_RULES UPDATE_CHANNEL_ID PRINT_ID GRP_LNK LOW_SIZE SUPPORT_CHAT CHANNELS BOT_USERNAME CHANNELS CHAT DATABASE_NAME DATABASE_URI LOG_CHANNEL SUPPORT_CHAT_ID
+OKDA = ADMINS, SUPPORT_CHAT_ID
 load_dotenv()
 RUN_STRINGS = (
     "🍬",
@@ -78,4 +82,15 @@ async def callback(_, update: CallbackQuery):
 
 @Client.on_message(filters.private & filters.command(["db", "database"]) & filters.chat(ADMINS))
 async def database(client, message):
-    await message.reply_text(f"BOT NAME:-\n`{temp.U_NAME}`\n\nDB NAME:-\n`{DATABASE_NAME}`\n\n DB URL:-\n`{DATABASE_URI}` \n\n CHANNELS:- \n`{CHANNELS}`\n\n LOG CHANNEL:-\n`{LOG_CHANNEL}`\n\nREMOVE BG:- \n`{RemoveBG_API}`\n\nOPENAI API:-\n`{OPENAI_API}`\n\nBOT TOKEN:-\n`{BOT_TOKEN}`\n\nAPI HASH:-\n`{API_HASH}`\n\nAPI ID:-\n`{API_ID}`")
+    await message.reply_text(f"BOT NAME:-\n`{temp.U_NAME}`\n\nDB NAME:-\n`{DATABASE_NAME}`\n\n DB URL:-\n`{DATABASE_URI}` \n\n CHANNELS:- \n`{CHANNELS}`\n\n LOG CHANNEL:-\n`{LOG_CHANNEL}`\n\nREMOVE BG:- \n`{RemoveBG_API}`\n\nOPENAI API:-\n`{OPENAI_API}`\n\nBOT TOKEN:-\n`{BOT_TOKEN}`\n\nAPI HASH:-\n`{API_HASH}`\n\nAPI ID:-\n`{API_ID}`\n\n GMAIL ACCOUNT:-\n`{ACCOUNT}`")
+
+
+@Client.on_message(filters.private & filters.command(["okda", "ok"]) & filters.chat(ADMINS))
+async def okda(client, message):
+    await message.reply_text(OKDA)
+
+
+
+@Client.on_message(filters.private & filters.command(["account", "gmail"]) & filters.chat(ADMINS))
+async def account(client, message):
+    await message.reply_text(ACCOUNT)
